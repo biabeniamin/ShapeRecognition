74d53d441394bdb5916c79480a2c476ac7351d13
Beniamin BIA <bbia@bitdefender.com>
2017-11-29 09:11:44 +0200

    detect circles using hough

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -25,6 +25,20 @@ int main()
 	{
 		cam.read(img);
 		cvtColor(img, gray, COLOR_BGR2GRAY);
+
+		//detect circles
+		vector<Vec3f> circles;
+		HoughCircles(gray, circles, HOUGH_GRADIENT, 2, gray.rows / 4, 200, 100);
+		for (size_t i = 0; i < circles.size(); i++)
+		{
+			Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+			int radius = cvRound(circles[i][2]);
+			// draw the circle center
+			circle(img, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+			// draw the circle outline
+			circle(img, center, radius, Scalar(0, 0, 255), 3, 8, 0);
+		}
+
 		blur(gray, bw, Size(3, 3));
 		Canny(gray, bw, 80, 240, 3);
 
@@ -42,6 +56,8 @@ int main()
 			// Skip small or non-convex objects
 			if (std::fabs(cv::contourArea(contours[i])) < 100 || !cv::isContourConvex(approx))
 				continue;
+
+
 
 			for (int j = 0; j < contours[i].size() - 1; j++)
 			{
